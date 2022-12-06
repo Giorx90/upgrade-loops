147555ebe00dbdc79ab2d76c9beffa06300cc64e
Jorge M <j.maldo.1990@gmail.com>
2022-12-06 20:01:43 +0100

    Iteration#2

---
+++ Ejercicio2.js
@@ -0,0 +1,13 @@
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+for(alumn of alumns) {
+    if ((alumn.T1 + alumn.T2 + alumn.T3) >= 2) {
+        console.log(alumn.name + " ha aprobado")
+    }
+}
